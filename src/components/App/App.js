e8a55cd35e0612181137aa86279da6a8331542b7
Shauna Myers <shaunamyers26@gmail.com>
2021-07-07 22:07:10 +0100

    Display movies from MoviesPoster component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,17 +7,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies;
+      movies: movieData.movies
     }
   }
-  return (
-    <main className="App">
-      <nav>
-        <h1>Rancid Tomatillos</h1>
-        <MoviesContainer/>
-      </nav>
-    </main>
-  );
+
+  render() {
+    return (
+      <main className="App">
+        <nav>
+          <h1>Rancid Tomatillos</h1>
+          <MoviesContainer movies={this.state.movies}/>
+        </nav>
+      </main>
+    );
+  }
 }
 
 export default App;
