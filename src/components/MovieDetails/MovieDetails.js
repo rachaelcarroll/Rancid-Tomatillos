35c9d66fffcd38596b8b5579438f61af18619507
Shauna Myers <shaunamyers26@gmail.com>
2021-07-08 15:45:56 +0100

    update deconstructed props in movie details component

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './MovieDetails.css'
 
-const MovieDetails = ({clickedMovie}) => {
+const MovieDetails = ({backgroundImage, posterImage, id, title, averageRating, releaseDate }) => {
     return (
-        <div className="movieDetails">
-            {clickedMovie}
-        </div>
+        <section className="MovieDetailsContainer">
+            <img src={backgroundImage}></img>
+            <div className="movieDetailsCard">
+                <img src={posterImage} id={id} alt={'Cover art image for ' + title}></img>
+            </div>
+        </section>
     )
 }
 
