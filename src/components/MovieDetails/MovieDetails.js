6c19c46c15697d2315a72e07b0197f1a670e9b8c
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-08 17:32:19 -0400

    Create variable for formatted rating

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './MovieDetails.css'
 
 const MovieDetails = ({backgroundImage, posterImage, id, title, averageRating, releaseDate, updateIsClicked }) => {
+    const formattedRating = averageRating.toFixed(0);
+    // const formattedDate = 
     return (
         <section className="MovieDetailsContainer">
             <img src={backgroundImage}></img>
-            <div className="movieDetailsCard">
+            <div className='movieDetailsCard'>
                 <img src={posterImage} id={id} alt={'Cover art image for ' + title}></img>
                 <button onClick={updateIsClicked}>X</button>
                 <h2>{title}</h2>
-                <p>{'Rating: ' + averageRating.toFixed(0) + '/10'}</p>
+                <p>{"Rating: " + formattedRating + "/10"}</p>
                 <p>{releaseDate}</p>
             </div>
         </section>
