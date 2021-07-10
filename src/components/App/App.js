67d138214984d1f1a7d44dc539f96891f8443b38
Shauna Myers <shaunamyers26@gmail.com>
2021-07-10 14:33:18 +0100

    Add error handling in render function in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,15 +11,15 @@ class App extends Component {
     super();
     this.state = {
       movies: [],
-      clickedMovie: {}
-      // error: ''
+      clickedMovie: {},
+      error: ''
     }
   }
 
   componentDidMount = () => {
     fetchMovies()
       .then(data => this.setState({ movies: data.movies }))
-      .catch(error => console.log(error));
+      .catch(error => this.setState({ error: 'Error loading movies...please try again.'} ));
   }
 
   resetClickedMovie = () => {
@@ -29,7 +29,7 @@ class App extends Component {
   updateClickedMovie = (id) => {
     fetchMovieInfo(id)
         .then(movie => this.setState({ clickedMovie: movie.movie }))
-        .catch(error => console.log(error));
+        .catch(error => this.setState({ error: 'Having trouble finding this movie right now...please try again.'} ));
   }
 
   render() {
@@ -38,9 +38,10 @@ class App extends Component {
         <nav>
           <h1>Rancid Tomatillos</h1>
         </nav>
-        {!this.state.movies.length && <h3>Loading...</h3>}
+        {this.state.error && <h3>{this.state.error}</h3>}
+        {!this.state.movies.length && !this.state.error && <h3>Loading...</h3>}
         {
-          this.state.clickedMovie.id ?
+          this.state.clickedMovie.id && !this.state.error ?
           <MovieDetails key={this.state.clickedMovie.id} movieInfo={this.state.clickedMovie} resetClickedMovie={this.resetClickedMovie}/> :
           <MoviesContainer 
           movies={this.state.movies} 
