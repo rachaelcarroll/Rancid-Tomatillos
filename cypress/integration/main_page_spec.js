daead3f3d67cc706520455d631ee450e5f6adc00
Shauna Myers <shaunamyers26@gmail.com>
2021-07-14 21:55:39 +0100

    Write test for inputting unique url for movie poster

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -22,12 +22,12 @@ describe ('Main Page', () => {
         cy.url().should('include', '/')
     })
 
-    it('Should be able to click on a mini-movie poster and be redirected to a new link', () => {
+    it('Should be able to click on a movie poster and be redirected to a new link', () => {
         cy
           .get('.moviesContainer').get("a[name='Mulan']").click()
           .url().should('include', '337401')
     });
-    
+
     it('Should see a movie details when clicking on a movie poster', () => {
         cy.get("a[name='Mulan']")
           .click()
@@ -36,6 +36,13 @@ describe ('Main Page', () => {
         .should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
     })
 
+    it('Should be able to enter a unique url with id in path and should render a single movie', () => {
+        cy.visit('http://localhost:3000/337401')
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+          fixture: 'single_movie_data.json' 
+        })
+        cy.get('.movieDescription').children('.movieDetailsCard').contains('Mulan')
+      })
 
 });
 
