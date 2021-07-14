693548efad2af3fed85c9640c310b734cc6aa0dd
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-14 13:25:52 -0400

    Add loading and error handling to render function

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -8,7 +8,8 @@ class MovieDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            movieInfo: {}
+            movieInfo: {},
+            error: ''
         }
     }
    
@@ -16,46 +17,55 @@ class MovieDetails extends Component {
         console.log("heyyyyyyyy", this.props.id);
         fetchMovieInfo(this.props.id)
             .then(movie => this.setState({ movieInfo: movie.movie }))
-            .catch(error => this.setState({ error: 'Having trouble finding this movie right now...please try again.'} ));
+            .catch(() => this.setState({ error: 'Having trouble finding this movie right now...please try again.'} ));
     }
 
-   
-    render() {
-
-
-    //     const formatOverviewLength = () => {
-    //     if (this.state.movieInfo.overview.length > 182) {
-    //         const shortOverview = this.state.movieInfo.overview.slice(0, 182) + '...';
+    // formatOverviewLength = (overview) => {
+    //     if (overview.length && overview.length > 20) {
+    //         const shortOverview = overview.slice(0, 20) + '...';
+    //         console.log('Shorty', shortOverview)
     //         return shortOverview;
     //     } else {
-    //         return this.state.movieInfo.overview;
+    //         console.log('OVERVIEW', overview)
+    //         return overview;
     //     }
     // }
-        if (!this.state.movieInfo) {
-            return <h2 className='loading-message'>Page Loading 🍿</h2>
-          }
+   
+    render() {
+        
+        if (this.state.movieInfo.id) {
+            const { movieInfo } = this.state
+            console.log("WORKING?", movieInfo)
+        }
 
-        return (
+            return (
+    
+                <section className="movieDetailsContainer" style={{ backgroundImage: `url(${this.state.movieInfo.backdrop_path})`}}>
+                    <Link to="/">
+                        <button className='returnHome'>X</button>
+                    </Link>
+                    {this.state.error && <h3>{this.state.error}</h3>}
+                    {!this.state.movieInfo && !this.state.error && <h2 className='loading-message'>Page Loading 🍿</h2>}
 
-            <section className="movieDetailsContainer" style={{ backgroundImage: `url(${this.state.movieInfo.backdrop_path})`}}>
-                <Link to="/">
-                    <button className='returnHome'>X</button>
-                </Link>
-                <div className='movieDescription'>
-                    <img className='movie-poster' src={this.state.movieInfo.poster_path} id={this.state.movieInfo.id} alt={'Cover art image for ' + this.state.movieInfo.title}></img>
-                    <div className='movieDetailsCard'>
-                        <h2>{this.state.movieInfo.title}</h2>
-                        <h4>{this.state.movieInfo.tagline}</h4>
-                        <p>{"Genre: " + this.state.movieInfo.genres}</p>
-                        {/* <p>{"Rating: " + formattedRating + "/10"}</p> */}
-                        <p>{this.state.movieInfo.release_date}</p>
-                        <p className='overview'>{this.state.movieInfo.overview}</p>
-                    </div>
-                </div>
-            </section>
-        )
-    }
+                    {this.state.movieInfo.id && !this.state.error &&
+                     <div className='movieDescription'>
+                        <img className='movie-poster' src={this.state.movieInfo.poster_path} id={this.state.movieInfo.id} alt={'Cover art image for ' + this.state.movieInfo.title}></img>
+                        <div className='movieDetailsCard'>
+                            <h2>{this.state.movieInfo.title}</h2>
+                            <h4>{this.state.movieInfo.tagline}</h4>
+                            <p>{"Genre: " + this.state.movieInfo.genres}</p>
+                            <p>{"Rating: " + this.state.movieInfo.average_rating + "/10"}</p>
+                            <p>{"Release Date: " + this.state.movieInfo.release_date}</p>
+                            <p className='overview'>{this.state.movieInfo.overview}</p>
+                        </div>
+                    </div> }
+                </section>
+            )
+
+    
+ }
 }
+
 MovieDetails.propTypes = {
     movieInfo: PropTypes.object,
     resetClickedMovie: PropTypes.func,
