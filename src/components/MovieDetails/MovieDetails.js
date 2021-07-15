efcd6996cca3fd9a758574d546e8d2864781238a
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-15 19:43:00 -0400

    Update format overview function, add format genre function

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -20,23 +20,27 @@ class MovieDetails extends Component {
             .catch(() => this.setState({ error: 'Having trouble finding this movie right now...please try again.'} ));
     }
 
-    // formatOverviewLength = (overview) => {
-    //     if (overview.length && overview.length > 20) {
-    //         const shortOverview = overview.slice(0, 20) + '...';
-    //         console.log('Shorty', shortOverview)
-    //         return shortOverview;
-    //     } else {
-    //         console.log('OVERVIEW', overview)
-    //         return overview;
-    //     }
-    // }
+    formatOverview = (overview) => {
+        if (overview.length > 185) {
+            const shortOverview = overview.slice(0, 185) + '...';
+            return shortOverview;
+        } else {
+            return overview;
+        }
+    }
+
+    formatGenres(genres) {
+        const allGenres = genres.join(' | ')
+        return allGenres 
+      }
+    
    
     render() {
         
-        if (this.state.movieInfo.id) {
-            const { movieInfo } = this.state
-            console.log("WORKING?", movieInfo)
-        }
+        // if (this.state.movieInfo.id) {
+        //     const { movieInfo } = this.state
+        //     console.log("WORKING?", movieInfo)
+        // }
 
             return (
     
@@ -53,10 +57,10 @@ class MovieDetails extends Component {
                         <div className='movieDetailsCard'>
                             <h2>{this.state.movieInfo.title}</h2>
                             <h4>{this.state.movieInfo.tagline}</h4>
-                            <p>{"Genre: " + this.state.movieInfo.genres}</p>
+                            <p>| {this.formatGenres(this.state.movieInfo.genres)} | </p>
                             <p>{"Rating: " + this.state.movieInfo.average_rating + "/10"}</p>
                             <p>{"Release Date: " + this.state.movieInfo.release_date}</p>
-                            <p className='overview'>{this.state.movieInfo.overview}</p>
+                            <p className='overview'>{this.formatOverview(this.state.movieInfo.overview)}</p>
                         </div>
                     </div> }
                 </section>
