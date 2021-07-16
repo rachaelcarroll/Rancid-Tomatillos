2f9793bebe6aa628ef669448800f104588fae357
Shauna Myers <shaunamyers26@gmail.com>
2021-07-16 11:10:10 +0100

    Move associated tests into movie_details_spec

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -3,14 +3,6 @@ describe ('Main Page', () => {
 
     beforeEach(() => {
       cy.seedAndVisit();
-        // cy.fixture('all_movie_data.json')
-        //   .then((movies) => {
-        //     cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-        //       statusCode: 201,
-        //       body: movies
-        //     })
-        //   })
-        // cy.visit('http://localhost:3000')
       })
         
     it('should be able to visit the app and display the header', () => {
@@ -23,35 +15,35 @@ describe ('Main Page', () => {
         cy.url().should('include', '/')
     })
 
-    it('Should be able to click on a movie poster and be redirected to a new link', () => {
-        cy
-          .get('.moviesContainer').get("a[name='Mulan']").click()
-          .url().should('include', '337401')
-    });
+    // it('Should be able to click on a movie poster and be redirected to a new link', () => {
+    //     cy
+    //       .get('.moviesContainer').get("a[name='Mulan']").click()
+    //       .url().should('include', '337401')
+    // });
 
-    it('Should see a movie details when clicking on a movie poster', () => {
-        cy.get("a[name='Mulan']")
-          .click()
-        cy.contains("Action")
-        cy.get("img")
-        .should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
-    })
+    // it('Should see a movie details when clicking on a movie poster', () => {
+    //     cy.get("a[name='Mulan']")
+    //       .click()
+    //     cy.contains("Action")
+    //     cy.get("img")
+    //     .should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
+    // })
 
-    it('Should be able to enter a unique url with id in path and should render a single movie', () => {
-        cy.visit('http://localhost:3000/337401')
-        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
-          fixture: 'single_movie_data.json' 
-        })
-        cy.get('.movieDescription').children('.movieDetailsCard').contains('Mulan')
-      })
+    // it('Should be able to enter a unique url with id in path and should render a single movie', () => {
+    //     cy.visit('http://localhost:3000/337401')
+    //     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+    //       fixture: 'single_movie_data.json' 
+    //     })
+    //     cy.get('.movieDescription').children('.movieDetailsCard').contains('Mulan')
+    //   })
 
-      it('Should return to home page view showing all movies when the exit button is clicked', () => {
-        cy
-        .get('.moviesContainer')
-        .get("a[name='Mulan']").click()
-        .get('button').click()
-        cy.url().should('include', '/')
-      })
+    //   it('Should return to home page view showing all movies when the exit button is clicked', () => {
+    //     cy
+    //     .get('.moviesContainer')
+    //     .get("a[name='Mulan']").click()
+    //     .get('button').click()
+    //     cy.url().should('include', '/')
+    //   })
 
 });
 
---
+++ cypress/integration/movie_details_spec.js
@@ -0,0 +1,37 @@
+describe ('Main Details Page', () => {
+
+    beforeEach(() => {
+        cy.seedAndVisit();
+        })
+
+    it('Should be able to click on a movie poster and be redirected to a new link', () => {
+        cy
+          .get('.moviesContainer').get("a[name='Mulan']").click()
+          .url().should('include', '337401')
+    });
+
+    it('Should see a movie details when clicking on a movie poster', () => {
+        cy.get("a[name='Mulan']")
+          .click()
+        cy.contains("Action")
+        cy.get("img")
+        .should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
+    })
+
+    it('Should be able to enter a unique url with id in path and should render a single movie', () => {
+        cy.visit('http://localhost:3000/337401')
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+          fixture: 'single_movie_data.json' 
+        })
+        cy.get('.movieDescription').children('.movieDetailsCard').contains('Mulan')
+      })
+
+      it('Should return to home page view showing all movies when the exit button is clicked', () => {
+        cy
+        .get('.moviesContainer')
+        .get("a[name='Mulan']").click()
+        .get('button').click()
+        cy.url().should('include', '/')
+      })
+
+})
