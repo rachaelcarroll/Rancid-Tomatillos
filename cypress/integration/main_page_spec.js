472bb69b4e8ae9935da8a2d41a74f7bd991885a9
Shauna Myers <shaunamyers26@gmail.com>
2021-07-18 22:03:08 +0100

    Write test for search field typing and movie display

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -6,22 +6,44 @@ describe ('Main Page', () => {
     })
         
     it('should be able to visit the app and display the header', () => {
-        cy.get('h1')
-        .contains('Rancid Tomatillos')
+      cy
+        .get('.nav')
+        .get('.header')
+        .contains('RANCID TOMATILLOS')
     })
 
     it('Should be able to visit the page and render all movies', () => {
-        cy.get('main').children('.moviesContainer')
-        cy.url().should('include', '/')
+      cy
+        .get('main')
+        .children('.moviesContainer')
+        .url()
+        .should('include', '/')
+    })
+
+    it('Should be able to type in the search field', () => {
+      cy
+        .get('input[type=text]')
+        .type('Mulan')
+        .should('have.value', 'Mulan')
+    })
+
+    it('Should be able to display only the movie(s) that match search criteria', () => {
+      cy
+        .get('input[type=text]')
+        .type('Av')
+        .get('.moviesContainer')
+        .get("img")
+        .should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//qzA87Wf4jo1h8JMk9GilyIYvwsA.jpg")
     })
 
     
     it('Should be able to be able to naviagate back and forward in browser', () => {
-      cy.visit('http://localhost:3000/337401')
-      cy.go('back')
-      cy.url().should('eq','http://localhost:3000/')
-      cy.go('forward')
-      cy.url().should('eq','http://localhost:3000/337401')
+      cy
+        .visit('http://localhost:3000/337401')
+        .go('back')
+        .url().should('eq','http://localhost:3000/')
+        .go('forward')
+        .url().should('eq','http://localhost:3000/337401')
     })
 });
 
