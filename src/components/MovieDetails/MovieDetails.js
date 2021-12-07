14d4e9e4fc71d81ba5d8a34b8764038149de6f20
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-12-07 12:55:10 -0500

    Update MovieDetails with hooks

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,125 +1,82 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './MovieDetails.css';
 import { Link } from 'react-router-dom';
 import { fetchMovieInfo, fetchVideo } from '../../apiCalls';
+import { formatOverview, formatGenres, formatCurrency, formatRating, formatReleaseDate, formatRuntime } from '../../util';
 
-class MovieDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movieInfo: {},
-            movieTrailers: '',
-            movieError: '',
-            videoError: '',
-        }
-    }
+const MovieDetails = ({ movieInfo }) => {
+    const [movie, setMovie] = useState({});
+    const [trailer, setTrailer] = useState('');
+    const [error, setError] = useState('');
    
-    componentDidMount() {
-        fetchMovieInfo(this.props.id)
-            .then(movie => this.setState({ movieInfo: movie.movie }))
-            .catch(() => this.setState({ movieError: 'Having trouble finding movie information right now...please try again.'} ));
-
-        fetchVideo(this.props.id)
-            .then(video => {this.setState({movieTrailers: video.videos})})
-            .catch(() => this.setState({ videoError: 'Sorry, this video is currently unavailable.'}))
-    }
-
-    formatOverview = (overview) => {
-        if (overview.length > 185) {
-            const shortOverview = overview.slice(0, 185) + '...';
-            return shortOverview;
-        } else {
-            return overview;
+    useEffect(() => {
+        const getMovieDetails = async (id) => {
+            try {
+            let data = await fetchMovieInfo(id);
+            setMovie(data.movie);
+          } catch (error) {
+            setError(error.message);
+          }
         }
-    }
+        getMovieDetails(movieInfo.id);
+      }, [movieInfo])
 
-    formatGenres = (genres) => {
-        if(genres) {
-            const allGenres = genres.join(' | ')
-            return allGenres 
-        } else {
-            return '';
+      useEffect(() => {
+        const getMovieTrailer = async (id) => {
+            try {
+            let data = await fetchVideo(id);
+            setTrailer(data.videos[0].key);
+          } catch (error) {
+            setError(error.message);
+          }
         }
-      }
-
-    formatRating = (rating) => {
-        const formattedRating = rating.toFixed(0)
-        return formattedRating
-    }
+        getMovieTrailer(movieInfo.id);
+      }, [movieInfo])
     
-    formatReleaseDate = (date) => {
-        const yearReleased = date.split("-")[0]
-        return yearReleased
-    }
-
-    formatCurrency = (amount) => {
-        if (amount === 0) {
-          return 'Not Reported'
-        }
-    
-        return amount.toLocaleString('en-US', {
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0,
-          style: 'currency',
-          currency: 'USD'
-        })
-      }
-
-    formatRuntime = (runtime) => {
-     return !runtime ? 'Not Reported' : runtime + " mins"
-    }
-
-
-    render() {
-        const { id, title, release_date, backdrop_path, poster_path, overview, genres, budget, revenue, tagline, average_rating, runtime} = this.state.movieInfo
-    
-            return (
-                <section>
-                    <section className="movieDetailsContainer" style={{ backgroundImage: `url(${backdrop_path})`}}>
-                        <Link to="/">
-                            <button className='returnHome' onClick={this.props.handleDisplayAllMovies}>X</button>
-                        </Link>
-                        {this.state.movieError && <h3 className="errorLoading">{this.state.movieError}</h3>}
-                        {!this.state.movieInfo && !this.state.error && <h2 className='loading-message'>Page Loading...</h2>}
-
-                        {title && !this.state.error &&
-                        <article className='movieDescription'>
-                            <img className='movie-poster' src={poster_path} id={id} alt={'Cover art image for ' + title}></img>
-                            <div className='movieDetailsCard'>
-                                <h2>{title}</h2>
-                                <h4>{tagline}</h4>
-                                <p className='genre'>{this.formatGenres(genres)}</p>
-                                <p><strong>Rating: </strong>{this.formatRating(average_rating)}/10</p>
-                                <p><strong>Year Released: </strong>{this.formatReleaseDate(release_date)}</p>
-                                <p className='overview'>{this.formatOverview(overview)}</p>
-                                <p className='budget'><strong>Budget: </strong>{this.formatCurrency(budget)}</p>
-                                <p className='revenue'><strong>Revenue: </strong>{this.formatCurrency(revenue)}</p>
-                                <p className='runtime'><strong>Runtime: </strong>{this.formatRuntime(runtime)}</p>
-                            </div>
-                        </article> }
-                        {this.state.videoError && <h3 className="errorLoading">{this.state.videoError}</h3>}
-                        {!!this.state.movieTrailers.length && 
-                            <div className='movieTrailer'>
-                                <iframe
-                                    title="Embedded youtube"
-                                    className='video'
-                                    width='650'
-                                    height='380'
-                                    src={`https://www.youtube.com/embed/${this.state.movieTrailers[0].key}`}
-                                    frameBorder='0'
-                                    allowFullScreen
-                                />
-                            </div> }
-                    </section>
-                </section>
-            )
-    }
+    return (
+        <section>
+            <section className="movieDetailsContainer" style={{ backgroundImage: `url(${movie.backdrop_path})`}}>
+                <Link to="/">
+                    <button className='returnHome'>X</button>
+                </Link>
+                {error && <h3 className="errorLoading">{error}</h3>}
+                {!movie && !error && <h2 className='loading-message'>Loading Movie Details...</h2>}
+                {movie.title && !error &&
+                <article className='movieDescription'>
+                    <img className='movie-poster' src={movie.poster_path} id={movie.id} alt={'Cover art image for ' + movie.title}></img>
+                    <div className='movieDetailsCard'>
+                        <h2>{movie.title}</h2>
+                        <h4>{movie.tagline}</h4>
+                        <p className='genre'>{formatGenres(movie.genres)}</p>
+                        <p><strong>Rating: </strong>{formatRating(movie.average_rating)}/10</p>
+                        <p><strong>Year Released: </strong>{formatReleaseDate(movie.release_date)}</p>
+                        <p className='overview'>{formatOverview(movie.overview)}</p>
+                        <p className='budget'><strong>Budget: </strong>{formatCurrency(movie.budget)}</p>
+                        <p className='revenue'><strong>Revenue: </strong>{formatCurrency(movie.revenue)}</p>
+                        <p className='runtime'><strong>Runtime: </strong>{formatRuntime(movie.runtime)}</p>
+                    </div>
+                </article> }
+                {error && <h3 className="errorLoading">{error}</h3>}
+                {trailer && 
+                    <div className='movieTrailer'>
+                        <iframe
+                            title="Embedded youtube"
+                            className='video'
+                            width='650'
+                            height='380'
+                            src={`https://www.youtube.com/embed/${trailer}`}
+                            frameBorder='0'
+                            allowFullScreen
+                        />
+                    </div> }
+            </section>
+        </section>
+    )
 }
 
 MovieDetails.propTypes = {
     movieInfo: PropTypes.object,
-    resetClickedMovie: PropTypes.func,
   }
 
 export default MovieDetails;
